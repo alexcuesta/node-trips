feeca0e1aee673513ba3de0ab7c6dcedfeb50c6f
Alejandro Cuesta <alejandro.cuesta@ocado.com>
2026-03-18 17:40:43 +0100

    Parameterirse tests

--- main-service/app.test.js
+++ main-service/app.test.js
@@ -36,14 +36,20 @@ describe('Get /trips', () => {
         ])
     })
 
-    it('should return trips sorted by price', async () => {
-        const response = await request(app).get('/trips?sort=price')
-        expect(response.status).toBe(200)
-        expect(response.body).toEqual([
-            { "id": 2, "destination": "Oviedo", "price": 50, "duration": 2 },    
+    it.each([
+        ['price', [
+            { "id": 2, "destination": "Oviedo", "price": 50, "duration": 2 },
             { "id": 1, "destination": "Madrid", "price": 100, "duration": 3 }
-        ])
+        ]],
+        ['duration', [
+            { "id": 2, "destination": "Oviedo", "price": 50, "duration": 2 },
+            { "id": 1, "destination": "Madrid", "price": 100, "duration": 3 }
+        ]]
 
+    ])('should return trips sorted by %s', async (sortField, expectedBody) => {
+        const response = await request(app).get(`/trips?sort=${sortField}`)
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(expectedBody)
     })
 
 })
